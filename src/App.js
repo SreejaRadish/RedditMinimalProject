309ab45cae3d2c6c732bb63e38f3c14fbc74290d
Sreeja R <SreejaRadish@gmail.com>
2021-10-05 14:54:58 +0100

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 
 import { useState,useEffect } from 'react';
 import './App.css';
-
+import  Items from './Components/Items';
 function App() {
   const [items,setItems] = useState([]);
-  const [query,setQuery] =useState('');
+  const [query,setQuery] =useState('popular');
+  const [search,setSearch] = useState('');
   useEffect(() => {
     getItems();
   },[query]);
   const getItems = async () => {
-    const response = await fetch("https://www.reddit.com/r/popular.json");
-   
+    const response = await fetch(`https://www.reddit.com/search.json?q=${query}`);
+   const url = `https://www.reddit.com/search.json?q=${query}`;
+   console.log(url);
     const data = await response.json();
     if(data != null){
       setItems(data.data.children);
@@ -18,16 +20,25 @@ function App() {
     
 console.log(data);
       }
+      const updateHandler = (e) => {
+        setSearch(e.target.value);
+      }
       const submitHandler = (e) => {
-
+      e.preventDefault();
+      setQuery(search);
+      setSearch('');
       }
   return (
     <div className="App">
-      <form className="search-form" >
-<input type="text"></input>
-<button type="submit" className="search-Button">Search</button>
-      </form>
+   <h3>Reddit<span className="headinSpan">Minimal</span></h3>
+<input type="text" value={search} onChange={updateHandler}></input>
+<button type="submit" className="search-Button" onClick={submitHandler}>Search</button>
+     
       <div className="itemWrapper">
+        <div className ="">
+      { 
+      items.map((item,index) => ( < Items key={index} item={item.data} />)) }
+</div>
 
       </div>
     </div>
--- src/Components/Items.js
+++ src/Components/Items.js
@@ -1,11 +1,26 @@
-import React from './react';
-
+import React from 'react';
+import Style from '../App.css';
 const Items = (props) => {
     return (
-        <div>
-          <h3>Title</h3>
+      
+        <div className="contentWrapper">
+          <div className="articleWrapper">
+            <article>
+              <a href={"https://reddit.com"+props.item.permalink}>
+              <h3>{props.item.title}</h3>
+              </a>
+            </article>
+         
+          {props.item.thumbnail !== "self" 
+           ?  <img className="imageStyle" src={props.item.thumbnail} alt="" height="400px" width="500px"></img> : ""         }
+         </div>
+         <div className="subredditWrapper">
+           <img  className="subredditImage" src="" alt=""></img>
+           <p>{props.item.subreddit}</p>
+         </div>
         </div>
-    );
+
+           );
 }
 
 
